a86d92914017fe91265dac5b34566c9120fc0467
Karan Singh <karansingh@adobe.com>
2025-05-16 11:52:53 +0530

    fixing the errors

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <assert.h>
+#include <vector>
+#include <string>
 
-int printColorMap() {
+std::vector<std::string> getColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+    std::vector<std::string> map;
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5; j++) {
+            char buffer[50];
+            snprintf(buffer, sizeof(buffer), "%d | %s | %s", i * 5 + j, majorColor[i], minorColor[j]);
+            map.push_back(buffer);
         }
     }
-    return i * j;
+    return map;
 }
 
 void testPrintColorMap() {
     std::cout << "\nPrint color map test\n"; 
-    int result = printColorMap();
-    assert(result == 25);
+    auto colorMap = getColorMap();
+    assert(colorMap.size() == 25);
+    assert(colorMap[0] == "0 | White | Blue");
+    assert(colorMap[24] == "24 | Violet | Slate");
     std::cout << "All is well (maybe!)\n";
 }
--- tshirts.cpp
+++ tshirts.cpp
@@ -16,7 +16,9 @@ char size(int cms) {
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
     assert(size(37) == 'S');
+    assert(size(38) == 'M'); // This will fail!
     assert(size(40) == 'M');
+    assert(size(42) == 'L'); // This will fail!
     assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
 }
